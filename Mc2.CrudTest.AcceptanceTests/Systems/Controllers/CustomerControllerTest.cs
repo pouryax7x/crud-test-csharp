5c487a803cdae2a2835bcd4ef9718b5383e563be
Pourya <Pourya.tag@gmail.com>
2022-07-06 20:24:30 +0430

    add new project test add controller test add customer controller

---
+++ Mc2.CrudTest.AcceptanceTests/Systems/Controllers/CustomerControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Mc2.CrudTest.Presentation.Server.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mc2.CrudTest.AcceptanceTests.Systems.Controllers
+{
+    public class CustomerControllerTest
+    {
+
+        [Fact]
+        public async Task GetOnSuccessReturnStatusCode200()
+        {
+            //arrange
+            var controller = new CustomerController();
+            //act
+            var result = (OkObjectResult) await  controller.GetCustomers();
+            //assert
+            result.StatusCode.Should().Be(200);
+        }
+    }
+}
---
+++ Mc2.CrudTest.Presentation/Server/Controllers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Presentation.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetCustomers()
+        {
+            return null;
+        }
+    }
+}
